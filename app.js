08fa5cec478bd54fc7d060d3148917dbece53d5c
uSilky <39656396+uSilky@users.noreply.github.com>
2018-06-25 20:16:05 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const config = require("./config.json");
+
+client.on("ready", () => {
+
+  console.log(`BOT ON!`); 
+let loop = require(`./lockfeio.js`);
+  loop.run(client);
+});
+
+client.login(config.token);
+
+client.on("message", async message => {
+
+    if(message.author.bot) return;
+    
+    if(message.content.indexOf(config.prefix) !== 0) return;
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+});
+
