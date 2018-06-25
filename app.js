2d86a0db4d0a65c4a635eae73d65285ddbbab11a
uSilky <39656396+uSilky@users.noreply.github.com>
2018-06-25 20:40:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -21,5 +21,27 @@ client.on("message", async message => {
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
+if(command === "say") {
+  message.delete().catch(O_o=>{});
+  if (message.member.hasPermission('MANAGE_GUILD')) {  
+         
+      let mensg = args.join(" ");
+      if(!mensg)
+         return message.channel.send("Digite a mensagem de aviso!")
+         
+      const say = new Discord.RichEmbed()
+         .setColor("0cff00")
+         .setAuthor("Anúncio", "https://cdn.discordapp.com/attachments/459214311521386497/460544721064820736/thinkingblackguy_1.png")
+         
+         .setDescription(mensg)
+         
+         .setTimestamp()
+         .setFooter(`Por: ${message.author.tag}` , message.author.avatarURL)
+      
+      message.channel.send("@everyone", say)
+      everyone.delete()
+  }
+}
+});
 });
 
